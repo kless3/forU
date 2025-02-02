f36ae835b982c0881c2385b6e0385fe44b3272a0
kless <hiklesd@gmail.com>
2025-02-03 02:11:33 +0300

    sdf

--- src/main/java/com/kless/forU/Application.java
+++ src/main/java/com/kless/forU/Application.java
@@ -2,9 +2,19 @@ package com.kless.forU;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class Application {
+
+    @GetMapping("/")
+    public String getMainPage(){
+        return "main";
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
