729894773ec5592abb74267e833d367e8ff89f46
kless <hiklesd@gmail.com>
2025-02-03 02:19:47 +0300

    sdf

--- src/main/java/com/kless/forU/Main.java
+++ src/main/java/com/kless/forU/Main.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
-public class Application {
+public class Main {
 
     @GetMapping("/")
     public String getMainPage(){
@@ -16,6 +16,6 @@ public class Application {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
