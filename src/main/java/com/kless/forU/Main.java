a9bc6957203453bc358bc6ffd30556a36b29159b
kless <hiklesd@gmail.com>
2025-02-03 02:33:17 +0300

    asdf

--- src/main/java/com/kless/forU/Main.java
+++ src/main/java/com/kless/forU/Main.java
@@ -6,14 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@Controller
 public class Main {
-
-    @GetMapping("/")
-    public String getMainPage(){
-        return "main";
-    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
---
+++ src/main/java/com/kless/forU/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.kless.forU.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/")
+    public String getMainPage(){
+        return "main";
+    }
+
+
+
+}
